657cc7340b9d6a70966066eab8ea7b5ab6d5b3bd
Lefrec <blackfox244@gmail.com>
2025-03-13 17:55:05 +0100

    changement du lien pocketbase

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('htts://cine-flash-25.paolo-vincent.fr:443');
 
 export async function allFilms() {
     try {
